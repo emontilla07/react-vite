732de440f49da1c761af4c0c7922424f1423a013
emontilla07 <emontilla107@gmail.com>
2023-04-05 16:40:25 -0500

    Intro React

---
+++ 02-intro-javascript/src/base/01-const-let.js
@@ -0,0 +1,12 @@
+const nombre = 'Eric';
+const apellido = 'Montilla';
+
+let valorDado = 5;
+valorDado = 4;
+
+console.log(nombre, apellido, valorDado);
+
+if (true) {
+    const nombre = 'Peter';
+    console.log(nombre);
+}
