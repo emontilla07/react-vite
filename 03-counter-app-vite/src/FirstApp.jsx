73847c47d599d43502840dfde5c94af315f52b90
emontilla07 <emontilla107@gmail.com>
2023-04-10 16:56:26 -0500

    CounterApp Project with React + Vite finish

---
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -0,0 +1,32 @@
+// const newMessage = {
+//   message: 'Hola Mundo',
+//   title: 'Eric',
+// };
+
+// const saludar = (a, b) => a * b;
+
+import PropTypes from 'prop-types';
+
+export const FirstApp = ({title, subTitle, name}) => {
+  return (
+    <>
+      <h1>{title}</h1>
+      {/* <h1>Resultado = {saludar(3, 5)}</h1> */}
+      {/* <h1>{JSON.stringify(newMessage)}</h1> */}
+      {/* <p>Soy un subtitulo</p> */}
+      <p>{subTitle}</p>
+      <p>{name}</p>
+    </>
+  );
+}
+
+FirstApp.propTypes = {
+  subTitle: PropTypes.string,
+  title: PropTypes.string.isRequired,
+};
+
+FirstApp.defaultProps = {
+  name: 'Eric Montilla',
+  subTitle: 'No hay sub título',
+  title: 'No hay título',
+};
