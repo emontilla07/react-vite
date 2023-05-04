f9447bf437f266e3bcca34b8d8fc095b65c92f8e
emontilla07 <emontilla107@gmail.com>
2023-05-04 16:44:24 -0500

    Clean main.jsx

--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import { HelloWorldApp } from './HelloWorldApp';
-import { FirstApp } from './FirstApp';
+// import { FirstApp } from './FirstApp';
 // import { CounterApp } from './CounterApp';
 import './styles.css';
 import { CounterApp2 } from './CounterApp2';
-// import { CounterApp } from './CounterApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
