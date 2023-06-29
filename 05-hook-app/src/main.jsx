d67d862811777cac458a0d1f1d5cc6362dd4e04b
emontilla07 <emontilla107@gmail.com>
2023-06-29 16:32:39 -0500

    intro useReducer

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -1,5 +1,5 @@
 // import React from 'react';
-import ReactDOM from 'react-dom/client';
+// import ReactDOM from 'react-dom/client';
 // import { HooksApp } from './HooksApp';
 // import { CounterApp } from './01-useState/CounterApp';
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
@@ -11,11 +11,12 @@ import ReactDOM from 'react-dom/client';
 // import { Memorize } from './06-memos/Memorize';
 // import { MemoHook } from './06-memos/MemoHook';
 // import { CallBackHook } from './06-memos/CallBackHook';
-import { Padre } from './07-tarea-memo/Padre';
+// import { Padre } from './07-tarea-memo/Padre';
+import './08-useReducer/intro-reducer';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// ReactDOM.createRoot(document.getElementById('root')).render(
     // <React.StrictMode>
-        <Padre />
+        // <Padre />
     // </React.StrictMode>
-)
+// )
